43e1e28cd461a0adb76e5468a2d51d0292f19902
Ayaka Neko <neko@ayaka.moe>
2021-03-02 15:09:36 +0800

    :fire: Reset all files
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <Button />
-    <el-button type="primary" plain>主要按钮</el-button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Button from '@/components/Button'
 
 export default {
   name: 'Home',
   components: {
-    Button
   }
 }
 </script>
