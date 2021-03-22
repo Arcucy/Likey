5db869a44597135c8b5d64ee2fdd129bf599ecc5
LittleSound <464388324@qq.com>
2021-03-22 23:39:31 +0800

    :sparkles: Flow 动态卡片

---
+++ src/components/FlowCard/MainText.vue
@@ -0,0 +1,45 @@
+<template>
+  <p class="dynamic-text" v-html="content || '&nbsp;'" />
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  props: {
+    // 卡片数据
+    card: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    content () {
+      return 'xxx'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.dynamic-text {
+  color: #333;
+  overflow: hidden;
+  width: 100%;
+  word-break: break-word;
+  white-space: pre-line;
+  line-height: 1.5;
+  display: -webkit-box;
+  -webkit-line-clamp: 20;
+  -webkit-box-orient: vertical;
+  em {
+    font-weight: bold;
+    font-style: normal;
+    color: rgb(47, 174, 227);
+  }
+  a {
+    color: rgb(47, 174, 227)
+  }
+}
+</style>
