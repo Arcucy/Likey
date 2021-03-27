346eba4b02c4452412a28b0e19900828d69a19cc
LemonNeko <chheese048@gmail.com>
2021-03-27 10:19:21 +0800

    完成主页"发现创作者"分栏

---
+++ src/components/CreatorCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <a :href="'/#/@' + shortname" target="_blank">
+    <div class="creator-card" v-loading="loading">
+      <avatar class="creator-card-avatar" size="48px" :src="avatar" />
+      <div style="display: inline-block" class="creator-card-info">
+        <span class="creator-card-info-name">{{ id }}</span><br>
+        <span class="creator-card-info-bio">{{ intro }}</span>
+      </div>
+    </div>
+  </a>
+</template>
+<script>
+import Avatar from '@/components/User/Avatar'
+import Api from '@/api/api'
+
+export default {
+  components: {
+    avatar: Avatar
+  },
+  props: {
+    address: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      id: 'Loading...',
+      intro: 'Loading...',
+      avatar: '',
+      shortname: ''
+    }
+  },
+  computed: {
+    loading () {
+      return this.id === 'Loading...' || this.avatar === '' || this.intro === 'Loading...'
+    }
+  },
+  async mounted () {
+    this.$store.dispatch('getCreatorInfo', this.address).then((result) => {
+      this.intro = result.intro
+      this.shortname = result.shortname
+    }).catch(e => {
+      console.log(e)
+    })
+    Api.gql.getAvatarByAddress(this.address).then((result) => {
+      this.avatar = result
+    })
+    // TODO:更换成graphQL
+    Api.arql.getIdFromAddress(this.address).then((result) => {
+      this.id = result.data
+    })
+  }
+}
+</script>
+<style lang="less" scoped>
+@import "../themes/variables";
+a {
+  text-decoration: none;
+  color: #000;
+}
+
+.creator-card {
+  background: @background;
+  box-shadow: 0 0 2px 0 #0000001a;
+  box-sizing: border-box;
+  border-radius: 6px;
+  padding: 20px;
+  margin-top: 10px;
+  display: flex;
+  align-items: center;
+  &-avatar {
+    margin-right: 10px;
+  }
+  &-info {
+    &-name {
+      font-weight: bold;
+    }
+  }
+}
+
+.el-loading-mask {
+  border-radius: 6px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,67 @@
 <template>
-  <h1>
-    Hi, Likey
-  </h1>
+  <el-main>
+    <el-row justify="center" type="flex">
+      <el-col :span="20">
+        <el-row :gutter="20">
+          <!--分栏：动态-->
+          <el-col :span="14">
+            <div class="home-timeline">
+              动态
+            </div>
+          </el-col>
+          <!--分栏：发现创作者-->
+          <el-col :span="10">
+            <div class="home-discovery">
+              <span class="home-discovery-title">发现创作者</span>
+              <creator-card v-for="(address, index) in Object.keys(contract.creators)" :address="address" :key="index" />
+            </div>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+  </el-main>
 </template>
 
 <script>
+import CreatorCard from '@/components/CreatorCard'
+import { mapState } from 'vuex'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    'creator-card': CreatorCard
+  },
+  computed: {
+    ...mapState(['contract'])
+  }
 }
 </script>
 
 <style lang="less" scoped>
+@import '../themes/variables';
+
+.card {
+  background: @background;
+  box-shadow: 0 0 2px 0 #0000001a;
+  box-sizing: border-box;
+  border-radius: 6px;
+  padding: 20px;
+}
+
+// 使卡片边框变得更明显
+// TODO:应当只在开发时使用
+.obviously-border {
+  border: 1px black solid;
+}
+
+.home-timeline {
+  // TODO
+}
+
+.home-discovery {
+  &-title {
+    font-size: 18px;
+    font-weight: bold;
+  }
+}
 </style>
