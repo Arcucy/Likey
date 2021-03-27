0fb77856cbe0ae2c428d8f25cae04a9aa8dd3930
LemonNeko <chheese048@gmail.com>
2021-03-27 18:46:14 +0800

    :sparkles:主页>发现创作者>添加"显示更多"按钮

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <el-main>
     <el-row justify="center" type="flex">
-      <el-col :span="20">
+      <el-col :span="24" class="content-container">
         <el-row :gutter="20">
           <!--分栏：动态-->
           <el-col :span="14">
@@ -13,7 +13,10 @@
           <el-col :span="10">
             <div class="home-discovery">
               <span class="home-discovery-title">发现创作者</span>
-              <creator-card v-for="(address, index) in Object.keys(contract.creators)" :address="address" :key="index" />
+              <creator-card v-for="(address, index) in shownCreators" :address="address" :key="index" />
+              <div class="show-more-btn">
+                <span @click="showMore ++" v-if="creators.length !== shownCreators.length" class="show-more-btn-text">显示更多</span>
+              </div>
             </div>
           </el-col>
         </el-row>
@@ -24,21 +27,40 @@
 
 <script>
 import CreatorCard from '@/components/CreatorCard'
-import { mapState } from 'vuex'
+import { mapMutations, mapState } from 'vuex'
 
 export default {
   name: 'Home',
   components: {
     'creator-card': CreatorCard
   },
+  data () {
+    return {
+      // 记录用户点了多少次展开
+      showMore: 1
+    }
+  },
   computed: {
-    ...mapState(['contract'])
+    ...mapState({
+      creators: state => Object.keys(state.contract.creators),
+      shownCreators () {
+        return this.creators.slice(0, this.showMore * 5)
+      }
+    })
+  },
+  methods: {
+    // TODO:在每次提交前记得删掉这个mutations的引用
+    ...mapMutations(['mTestCreatorsAdd'])
   }
 }
 </script>
 
 <style lang="less" scoped>
 @import '../themes/variables';
+
+.content-container {
+  max-width: 1200px;
+}
 
 .card {
   background: @background;
@@ -62,6 +84,20 @@ export default {
   &-title {
     font-size: 18px;
     font-weight: bold;
+  }
+}
+
+.show-more-btn {
+  width: 100%;
+  text-align: center;
+  padding: 10px;
+  &-text {
+    transition: all 200ms;
+    font-weight: bold;
+    color: @primary;
+    &:hover {
+      color: @secondary;
+    }
   }
 }
 </style>
