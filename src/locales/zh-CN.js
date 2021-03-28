3337d189579bfb4784136c0eee4a54157155abe9
LittleSound <464388324@qq.com>
2021-03-29 00:38:52 +0800

    🔀 解决合并冲突

--- src/locales/zh-CN.js
+++ src/locales/zh-CN.js
@@ -35,7 +35,7 @@ export default {
     dynamicEncryptFailed: '动态加密失败',
     dynamicEncryptFailedMessage: '动态加密失败，出现了重大严重错误，如果屡次出现请反馈指开发者',
     shouldnotSponsorYourSelf: '不能自己赞助以及打赏自己',
-    youCanOnlyPayForYourSelf: '你只能使用与你当前登录地址相同的钱包进行支付'
+    youCanOnlyPayForYourSelf: '你只能使用与你当前登录地址相同的钱包进行支付',
     getStatusDetails: '无法获取动态详情'
   },
   app: {
