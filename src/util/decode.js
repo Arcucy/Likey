4f1cc79fde0c2751842c8aa348491c5ec6376a44
LittleSound <464388324@qq.com>
2021-03-29 20:40:23 +0800

    🎨 把 convertPstToWinston 和 getRatio 放进 util 工具箱

--- src/util/decode.js
+++ src/util/decode.js
@@ -1,3 +1,5 @@
+import BigNumber from 'bignumber.js'
+
 const Decode = {
   /**
    * 将 Uint8Array 类型转换成 String
@@ -32,7 +34,44 @@ const Decode = {
       }
     }
     return out
+  },
+
+  /** 转换 PST 为 Winston */
+  convertPstToWinston (value, ratio) {
+    const { from, to } = Decode.getRatio(ratio)
+    value = new BigNumber(String(value)).multipliedBy(to).div(from).multipliedBy(1000000000000)
+    value = value.toFixed(12)
+
+    if (value === 'Infinity' || value === 'NaN') {
+      return '0'
+    }
+    return value
+  },
+
+  /** 拆分换算比率 */
+  getRatio (ratio) {
+    if (!/^1:\d*\.?\d*$/.test(ratio)) {
+      return { from: '1', to: '0' }
+    }
+    let from = 1
+    let to = parseFloat(parseFloat(ratio.split(':').pop()).toFixed(12))
+    let iteration = 0
+
+    while (true) {
+      if (!Number.isInteger(to)) {
+        to = to * 10
+        iteration++
+        continue
+      }
+      break
+    }
+
+    for (let i = 0; i < iteration; i++) {
+      from = new BigNumber(from).multipliedBy(10)
+    }
+    to = BigNumber(to)
+    return { from: new BigNumber(String(from)), to }
   }
 }
 
-module.exports = Decode
+export default Decode
