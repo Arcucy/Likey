53a5f52de83239fbc757d35839c4e47e27672260
LittleSound <464388324@qq.com>
2021-03-30 19:55:03 +0800

    ♻️ 升级 decode.uint8ArrayToString 小工具，支持转码 Emoji

--- src/util/decode.js
+++ src/util/decode.js
@@ -5,36 +5,39 @@ const Decode = {
    * 将 Uint8Array 类型转换成 String
    * @param {Uint8Array} array 需要转换的数组
    */
-  uint8ArrayToString (array) {
-    var out, i, len, c
-    var char2, char3
-    out = ''
-    len = array.length
-    i = 0
-    while (i < len) {
-      c = array[i++]
-      switch (c >> 4) {
-        case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-          // 0xxxxxxx
-          out += String.fromCharCode(c)
-          break
-        case 12: case 13:
-          // 110x xxxx   10xx xxxx
-          char2 = array[i++]
-          out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F))
-          break
-        case 14:
-          // 1110 xxxx  10xx xxxx  10xx xxxx
-          char2 = array[i++]
-          char3 = array[i++]
-          out += String.fromCharCode(((c & 0x0F) << 12) |
-            ((char2 & 0x3F) << 6) |
-            ((char3 & 0x3F) << 0))
-          break
+  uint8ArrayToString: (function () {
+    var charCache = new Array(128) // Preallocate the cache for the common single byte chars
+    var charFromCodePt = String.fromCodePoint || String.fromCharCode
+    var result = []
+
+    return function (array) {
+      var codePt, byte1
+      var buffLen = array.length
+
+      result.length = 0
+
+      for (var i = 0; i < buffLen;) {
+        byte1 = array[i++]
+
+        if (byte1 <= 0x7F) {
+          codePt = byte1
+        } else if (byte1 <= 0xDF) {
+          codePt = ((byte1 & 0x1F) << 6) | (array[i++] & 0x3F)
+        } else if (byte1 <= 0xEF) {
+          codePt = ((byte1 & 0x0F) << 12) | ((array[i++] & 0x3F) << 6) | (array[i++] & 0x3F)
+        } else if (String.fromCodePoint) {
+          codePt = ((byte1 & 0x07) << 18) | ((array[i++] & 0x3F) << 12) | ((array[i++] & 0x3F) << 6) | (array[i++] & 0x3F)
+        } else {
+          codePt = 63 // Cannot convert four byte code points, so use "?" instead
+          i += 3
+        }
+
+        result.push(charCache[codePt] || (charCache[codePt] = charFromCodePt(codePt)))
       }
+
+      return result.join('')
     }
-    return out
-  },
+  })(),
 
   /** 转换 PST 为 Winston */
   convertPstToWinston (value, ratio) {
