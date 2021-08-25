375fa7c739beedebb3142d667c5e4348ec3b37aa
Ayaka Neko <neko@ayaka.moe>
2021-08-25 13:47:59 +0800

    ⬆️ 升级 webpack
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,13 @@ module.exports = {
   },
 
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser'
   },
 
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prefer-regex-literals': 'off',
     'vue/singleline-html-element-content-newline': ['error', {
       ignores: ['p', 'span', 'time', 'a']
     }],
